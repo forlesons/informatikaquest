c3b5f1a0ab447a4ab39be88839d35c56793eccd1
forlesons <159321599+forlesons@users.noreply.github.com>
2024-09-28 23:09:34 +0300

    Add files via upload

---
+++ quest/questsJsScript.js
@@ -0,0 +1,12 @@
+document.cookie = "username=MaksNosov; expires=Thu, 18 Dec 2024 12:00:00 UTC; path=/";
+
+// возвращает куки с указанным name,
+// или undefined, если ничего не найдено
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+alert(getCookie("MaksNosov"));
