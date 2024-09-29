bb2b5cce4f338741dfba765e6148fb2e329e9665
forlesons <159321599+forlesons@users.noreply.github.com>
2024-09-29 10:56:00 +0300

    Add files via upload

--- quest/questsJsScript.js
+++ quest/questsJsScript.js
@@ -1,13 +1,6 @@
+var CompletedQuests = "";
+
 document.cookie = "completedQuests=; expires=Tue, 18 Dec 2024 12:00:00 UTC; path=/";
-
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-alert(getCookie("user"));
 
 function setCookie(name, value, options = {}) {
 
@@ -34,8 +27,6 @@ function setCookie(name, value, options = {}) {
     document.cookie = updatedCookie;
   }
   
-  // Пример использования:
-  setCookie('completedQuests', '4', {secure: true, 'max-age': 3600});
 
 // возвращает куки с указанным name,
 // или undefined, если ничего не найдено
@@ -46,4 +37,28 @@ function getCookie(name) {
     return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
+if (getCookie("CompletedQuests") == "1") {
+  document.getElementById("QuestOne").style.backgroundColor = "green"
+  CompletedQuests = "1";
+}
+if (getCookie("CompletedQuests") == "1,2") {
+  document.getElementById("QuestOne").style.backgroundColor = "green"
+  document.getElementById("QuestTwo").style.backgroundColor = "green"
+  CompletedQuests = "1,2";
+}
+if (getCookie("CompletedQuests") == "1,2,3") {
+  document.getElementById("QuestOne").style.backgroundColor = "green"
+  document.getElementById("QuestTwo").style.backgroundColor = "green"
+  document.getElementById("QuestThree").style.backgroundColor = "green"
+  CompletedQuests = "1,2,3";
+}
+
 alert(getCookie("completedQuests"));
+
+document.getElementById("CheckButton").onclick = function() {
+    if (document.getElementById("InputFromQuest1").value == "Видеокарта") {
+      document.getElementById("VideocardImg").className = "VideocardImgEnd";
+      setCookie('completedQuest', '1', {secure: true, 'max-age': 3600})
+      document.getElementById("QuestOne").style.backgroundColor = "green"
+    }
+}
