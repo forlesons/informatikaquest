ba8f6bb85c32970a2f5ef0a755be1739a9fdfff8
forlesons <159321599+forlesons@users.noreply.github.com>
2024-09-30 07:39:03 +0300

    Update questsJsScript.js

--- questsJsScript.js
+++ questsJsScript.js
@@ -1,13 +1,4 @@
-document.cookie = "completedQuests=; expires=Tue, 18 Dec 2024 12:00:00 UTC; path=/";
-
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-alert(getCookie("user"));
+var CompletedQuests = new Array();
 
 function setCookie(name, value, options = {}) {
 
@@ -34,8 +25,6 @@ function setCookie(name, value, options = {}) {
     document.cookie = updatedCookie;
   }
   
-  // Пример использования:
-  setCookie('completedQuests', '4', {secure: true, 'max-age': 3600});
 
 // возвращает куки с указанным name,
 // или undefined, если ничего не найдено
@@ -46,4 +35,43 @@ function getCookie(name) {
     return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
+try {
+  getCookie("completedQuests")
+} catch(a) {
+  document.cookie = "completedQuests=False,False,False,False,False,False,False,False,False; expires=Tue, 18 Dec 2024 12:00:00 UTC; path=/";
+}
+
+for (var i=0; i<9; i++) {
+  CompletedQuests[i] = getCookie("completedQuests").toString().split(",")[i];
+}
+
+alert(CompletedQuests);
+
+if (CompletedQuests[0] == "True") {
+  document.getElementById("QuestOne").style.backgroundColor = "green"
+    alert("ok")
+}
+if (CompletedQuests[1] == "True") {
+  document.getElementById("QuestTwo").style.backgroundColor = "green"
+}
+if (CompletedQuests[2] == "True") {
+  document.getElementById("QuestThree").style.backgroundColor = "green"
+}
+
 alert(getCookie("completedQuests"));
+
+document.getElementById("CheckButton").onclick = function() {
+    if (document.getElementById("InputFromQuest1").value == "Видеокарта" && CompletedQuests[0] == "False") {
+      document.getElementById("VideocardImg").src = "quest/materinskayaplata.jpg";
+      //setCookie('completedQuests', 'True,False,False,False,False,False,False,False,False', {secure: true, 'max-age': 3600})
+      document.getElementById("QuestOne").style.backgroundColor = "green"
+      CompletedQuests[0] = "True"
+    }
+    if (document.getElementById("InputFromQuest1").value == "Материнская плата" && CompletedQuests[0] == "True") {
+      //document.getElementById("VideocardImg").src = "quest/materinskayaplata.jpg";
+      //setCookie('completedQuests', 'True,False,False,False,False,False,False,False,False', {secure: true, 'max-age': 3600})
+      document.getElementById("QuestTwo").style.backgroundColor = "green"
+      CompletedQuests[1] = "True"
+    }
+}
+alert(CompletedQuests)
